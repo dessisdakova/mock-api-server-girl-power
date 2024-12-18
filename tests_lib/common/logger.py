6756b08910280081e22ed72b9dc7986729236abc
Vlada Radchenko <ladon95@gmail.com>
2024-12-18 20:22:15 +0200

    Logger has been created

--- tests_lib/common/logger.py
+++ tests_lib/common/logger.py
@@ -1,27 +1,28 @@
 import logging
+import os
 from pathlib import Path
+from tests_lib.common.yaml_loaders import load_config
 
-
-def create_logger(logger_name: str, log_file: str):
+def create_logger(logger_name: str, log_file_name: str):
     """
-    Creates a logger with INFO level, a file handler, and the specified formats.
+    Creates a logger with INFO level(set up in config/common_config.yaml), a file handler.
 
     :param logger_name: name of the logger - string.
-    :param log_file: name if the file that saves logs - string.
-    :return: configured logger instance.
+    :param log_file_name: the name of log file - string.
+    :return: logger instance.
     """
+    config = load_config("common_config")
+    log_dir = config["log_file_dir"]
+    log_level_str = config["log_level"]
+
     logger = logging.getLogger(logger_name)  # create custom logger
-    logger.setLevel(logging.INFO)  # set level fo INFO
-
-    # set the logs/ folder as default for log files and add the provided name for the file
-    logger_path = Path(__file__).resolve().parent.parent.parent / "logs" / log_file
-
+    logger.setLevel(logging.getLevelNamesMapping()[log_level_str])
+    log_file_path = os.path.join(log_dir, log_file_name)
     # create a file handler to save logs in a file
-    handler = logging.FileHandler(logger_path)
+    handler = logging.FileHandler(log_file_path)
 
     # set the format for logs saved in the file
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s -  %(filename)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
     handler.setFormatter(formatter)  # set the desired format for logs in the file
 
     # add file handler to logger
