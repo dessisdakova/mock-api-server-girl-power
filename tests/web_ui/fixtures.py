29e9c887c1b9041afd2d48c3c5dec535d8e0abbb
Desislava Dakova <dessisdakova@gmail.com>
2025-01-07 11:29:06 +0200

    old fixtures file

--- tests/web_ui/fixtures.py
+++ tests/web_ui/fixtures.py
@@ -9,42 +9,30 @@ from typing import Generator
 
 
 @pytest.fixture(scope="session")
-def config() -> dict:
-    """
-    Fixture to load and provide the web UI configuration for the test session.
-
-    :return: A dictionary containing the loaded configuration.
-    """
-    return load_config("web_ui_config")
-
-
-@pytest.fixture(scope="session")
-def logger(config) -> CustomLogger:
+def logger() -> CustomLogger:
     """
     Fixture to provide a logger for web UI tests.
 
-    :param config: Config fixture.
     :return: A CustomLogger instance for logging test-related information.
     """
-    log_file_name = config["log_file_name"]
-    log = CustomLogger("web_ui_logger", log_file_name)
+    log = CustomLogger("web_ui_logger", "web_ui_tests.log")
     return log
 
 
 @pytest.fixture(scope="function")
-def driver(config, logger, request) -> Generator[webdriver.Remote, None, None]:
+def driver(logger, request) -> Generator[webdriver.Remote, None, None]:
     """
     Fixture to initialize a web driver for UI tests based on the configuration.
 
     This fixture sets up the selected browser (Chrome, Firefox, or Edge) in headless mode
     and creates a remote WebDriver instance. It also sets an implicit wait time for elements.
 
-    :param config: Config fixture.
-    :param logger: Logger fixture.
-    :param request: Request object providing test context information.
-    :return: A web driver instance to be used for the duration of the test session.
+    :param logger: Logger fixture to log browser type.
+    :param request: Request object providing test information.
+    :return: A web driver instance to be used for the duration of each test.
     :raises TypeError: If the specified browser is not supported.
     """
+    config = load_config("web_ui_config")
 
     if config["browser"] == "chrome":
         options = ChromeOptions()
@@ -73,5 +61,4 @@ def driver(config, logger, request) -> Generator[webdriver.Remote, None, None]:
 
     logger.debug(f"WebDriver is closed.")
     logger.add_divider()
-
 
