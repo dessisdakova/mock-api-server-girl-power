b6e8697153d72a9c5b6d3248744234545f190b1e
Desislava Dakova <dessisdakova@gmail.com>
2025-01-08 17:06:59 +0200

    Renamed file, changed class to simplify  __init__ method

--- tests_lib/common/custom_logger.py
+++ tests_lib/common/custom_logger.py
@@ -1,49 +1,74 @@
-import logging
-import os
+import shutil
+
 from tests_lib.common.yaml_loaders import load_config
+import logging
+from pathlib import Path
 
 
 class CustomLogger(logging.Logger):
     """
     A custom logger class for managing test logs with extended functionality.
-
-    This class extends the base logging.Logger to provide preconfigured logging settings,
-    including a log file handler, a custom log format, and a utility method to add dividers
-    between log sessions for better readability.
     """
     def __init__(self, logger_name: str, log_file_name: str):
         """
-        Overrides the base class constructor to configure a custom logger.
-
-        Sets up a log file handler with a specified file name, applies a custom format, and
-        stores logs in the "/opt/project/logs" folder. The logger level is set to in config/common_config.yaml
+        Initializes a custom logger with a file handler and custom formatter.
 
         :param logger_name: Name of the logger instance.
-        :param log_file_name: Name of the log file where logs will be saved.
+        :param log_file_name: Name of the log file.
         """
-        config = load_config("common_config")
-        super().__init__(logger_name, config["log_level"])
+        # Create a logger
+        super().__init__(logger_name, self._get_log_level())
 
-        log_dir = config["log_file_dir"]
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-        self.log_file_path = os.path.join(log_dir, log_file_name)
+        # Set up log file path
+        self.log_file_path = self._get_log_file_path(log_file_name)
 
-        # create a file handler
+        # Set up the file handler and add it to the logger
+        file_handler = self._create_file_handler()
+        self.addHandler(file_handler)
+
+    def _get_log_level(self) -> str:
+        """
+        Gets the log level from a config file.
+
+        :return: String representation of the level.
+        """
+        config = load_config("logger_config")
+        return config["log_level"]
+
+    def _get_log_file_path(self, log_file_name: str) -> Path:
+        """
+        Constructs the path to the log file.
+
+        :param log_file_name: Name of the log file.
+        :return: Full path to the log file.
+        """
+        logs_folder = Path(__file__).resolve().parent.parent.parent / "logs"
+        return logs_folder / log_file_name
+
+    def _create_file_handler(self) -> logging.FileHandler:
+        """
+        Creates a file handler for logging to the log file.
+
+        :return: Configured file handler.
+        """
         handler = logging.FileHandler(self.log_file_path)
+        handler.setFormatter(self._create_formatter())
+        return handler
 
-        # create a formatter
-        formatter = logging.Formatter(
-            "%(asctime)s | %(levelname)s | %(name)s | %(filename)s | %(message)s", datefmt="%Y/%m/%d %H:%M:%S")
-        handler.setFormatter(formatter)
+    def _create_formatter(self) -> logging.Formatter:
+        """
+        Creates a custom formatter for log messages.
 
-        # add handler the created custom logger
-        self.addHandler(handler)
+        :return: Configured formatter.
+        """
+        return logging.Formatter(
+            "%(asctime)s | %(levelname)-5s | %(name)-s | %(filename)s | %(message)s",
+            datefmt="%Y/%m/%d %H:%M:%S"
+        )
 
     def add_divider(self) -> None:
         """
         Append a divider line to the log file separating log sessions for better readability.
-
         """
         with open(self.log_file_path, "a") as log_file:
-            log_file.write("-------------------------------------------------------------------\n")
+            log_file.write("----------------------------------------------------------------------------------------\n")
